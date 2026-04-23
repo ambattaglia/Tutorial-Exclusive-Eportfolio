1cb8b6c9a08b56a19674f188c8f7ce10d6ba39f7
ambattaglia <am.battaglia@yahoo.com>
2026-04-23 09:19:11 -0400

    updates

---
+++ index.js
@@ -0,0 +1,63 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+const shapes = document.querySelectorAll('.shape');
+const x = event.clientX * scaleFactor;
+const y = event.clientY * scaleFactor;
+
+for (let i = 0; i < shapes.length; i++) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+}
+}
+
+
+function toggleContrast() {
+contrastToggle = !contrastToggle;
+if (contrastToggle) {
+    document.body.classList += ' dark-theme';
+} 
+else {
+    document.body.classList.remove('dark-theme');
+}
+}
+
+function contact() {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');   
+    loading.classList += ' modal__overlay--visible';
+    
+    emailjs
+    .sendform(
+    'service_t9tslvy', 
+    'template_5gsll2d',
+     event.target,
+     "JJGNsYo3P-s4VyWw9"
+    )
+
+    .then(() => {
+    loading.classList.remove('modal__overlay--visible');
+    success.classList += ' modal__overlay--visible';
+
+    }).catch(() => {
+        loading.classList.remove('modal__overlay--visible');
+        alert(
+            'The email service is currently unavailable. Please contact me directly at your_email@example.com'
+        );
+    });
+}
+ 
+
+function toggleModal() {
+if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove('modal--open');
+}   
+isModalOpen = true; 
+document.body.classList.toggle('modal--open');
+}
+
